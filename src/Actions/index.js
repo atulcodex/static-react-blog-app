77d093bcc89b571d0381aa213a304ded765d47d6
Atul Prajapati <atulkprajapati2000@gmail.com>
2019-10-25 21:48:28 +0530

    imported jsonplaceholder component in action creator and assigned a variable response and assigned response to payload

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import JsonPlaceholder from '../APIS/JsonPlaceholder';
+
+export const fetchPosts = async () => {
+
+    const response = await JsonPlaceholder.get('/posts');
     return {
-        type: 'FETCH_POST'
+        type: 'FETCH_POST',
+        payload: response
     };
 };
