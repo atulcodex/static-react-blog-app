8970e5ee5a2502783c366c39b4faa01998536136
Atul Prajapati <atulkprajapati2000@gmail.com>
2019-10-27 11:07:10 +0530

    i am getting error of middleware and simple objects after adding async await in our action creator file that's why now i am using promise instead of response

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -2,9 +2,9 @@ import JsonPlaceholder from '../APIS/JsonPlaceholder';
 
 export const fetchPosts = async () => {
 
-    const response = await JsonPlaceholder.get('/posts');
+    const promise = await JsonPlaceholder.get('/posts');
     return {
         type: 'FETCH_POST',
-        payload: response
+        payload: promise
     };
 };
