017b9a5da0e64cff4cc92cb5a2657ee75ff2a441
Atul Prajapati <atulkprajapati2000@gmail.com>
2019-10-29 10:50:01 +0530

    we don't need entire response value we just want data from that that's why we written here response.data only

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -3,5 +3,5 @@ import JsonPlaceholder from '../APIS/JsonPlaceholder';
 export const fetchPosts = () => async (dispatch) => {
     const response = await JsonPlaceholder.get('/posts');
     
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
