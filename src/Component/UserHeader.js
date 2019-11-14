29756e9c8560397144029787cd8109bdbd52bf2c
Atul Prajapati <atulkprajapati2000@gmail.com>
2019-11-14 21:33:14 +0530

    i have just removed code from UserHeader component which fetch user data from API because i m already fetching user details from action creator file so don't need this extra code

--- src/Component/UserHeader.js
+++ src/Component/UserHeader.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { fetchUser } from '../Actions';
 import { connect } from 'react-redux';
 
 class UserHeader extends React.Component{
-
-    componentDidMount()
-    {
-        this.props.fetchUser(this.props.userId);
-    }
-
     render()
     {
         const { user } = this.props;
@@ -28,4 +21,4 @@ const mapStateToProps = (state, ownProps ) => {
     return { user: state.users.find( user => user.id === ownProps.userId ) };
 };
 
-export default connect(mapStateToProps, {fetchUser})(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
